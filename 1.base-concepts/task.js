c7430b6be05cae3c4d93a1b9f1172815f228b881
Aleksandr Mamontov <mamont861@yandex.ru>
2024-05-06 23:28:54 +0300

    1.base-concepts update

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,16 +1,12 @@
 'use strict';
 function solveEquation(a, b, c) {
-  let d;
-  d = (b * b) - (4 * a * c);
+  const discriminant = (b * b) - (4 * a * c);
   let arr = [];
-  if (d < 0) {
-    arr = [];
-    } else if (d === 0) {
+  if (discriminant === 0) {
     arr = [-b / (2 * a)];
-    } else if (d > 0) {
-      arr = [(-b + Math.sqrt(d) )/(2 * a), (-b - Math.sqrt(d) )/(2 * a)];
+    } else if (discriminant > 0) {
+      arr = [(-b + Math.sqrt(discriminant) )/(2 * a), (-b - Math.sqrt(discriminant) )/(2 * a)];
   }
-  
   return arr;
 }
 
@@ -21,7 +17,7 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
     let monthly = S * (P + (P / (((1 + P)**n) - 1)));
     let total = monthly * countMonths;
     if (total != 0) {
-      return total.toFixed(2);
+      return +total.toFixed(2);
     } else {
       total = 0;
       return total;
